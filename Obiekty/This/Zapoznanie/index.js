ea346078262be327709504b40b5479043736c00f
Mateusz Pawlowski <mateuszpawlowski29@gmail.com>
2023-12-28 19:09:53 +0100

    This

--- Obiekty/This/Zapoznanie/index.js
+++ Obiekty/This/Zapoznanie/index.js
@@ -64,35 +64,107 @@
 // person.address.showAddres2()
 // console.log(person.address.log)
 
-const personA = {
-    name: 'Mateusz'
-}
+// const personA = {
+//     name: 'Mateusz'
+// }
 
-const person = {
-    name: 'Weronika',
-    surName: {
-        first: 'pawłowicz',
-        second: 'peasfas'
-    },
-    showName(surname, age) {
-        console.log(this.name, this.surname ? this.surname : surname, this.age ? this.age : age)
-    }
-}
+// const person = {
+//     name: 'Weronika',
+//     surName: {
+//         first: 'pawłowicz',
+//         second: 'peasfas'
+//     },
+//     showName(surname, age) {
+//         console.log(this.name, this.surname ? this.surname : surname, this.age ? this.age : age)
+//     }
+// }
 
 
 
-const people = [
-    {
-        name: 'Mateusz',
-        surname: 'Pawłowski'
-    },
-    {
-        name: 'Weronika'
-    },
-]
+// const people = [
+//     {
+//         name: 'Mateusz',
+//         surname: 'Pawłowski'
+//     },
+//     {
+//         name: 'Weronika'
+//     },
+// ]
 
 // people.forEach(onePesron =>{
 //     const startFunction = person.showName.bind(onePesron, 'Kowalski', 10) 
 //     startFunction()
 // })
 
+// const animals = {
+//     array : ["kot", "pies", "chomik"],
+//     i: 0,
+//     btn: '#animals',
+//     bindButton() {
+//         const btn = document.querySelector(this.btn);
+//         btn.addEventListener("click", () => {
+//             console.log(this.array[this.i]);
+//             this.i++
+//         });
+//     }
+// }
+
+// const humans = {
+//     array: ["Mateusz", "Weronika", "Piotrek"],
+//     i: 0,
+//     btn: '#humans',
+// }
+
+// const btnA = document.querySelector("#activateA")
+// const btnH = document.querySelector("#activateH")
+
+// const activateButtons = [btnA, btnH]
+
+// const checkButton = (button) => {
+//     button.addEventListener('click', () => {
+//         const context = button.id == 'activateA' ? animals : humans
+//         animals.bindButton.call(context)
+//     })
+// }
+
+// activateButtons.forEach(checkButton)
+
+
+// const ob = {
+//     pets : ["kot", "pies", "chomik"],
+
+//     showPets() {
+//         console.log(this.pets);
+//     },
+
+//     bindButton() {
+//         this.showPets = this.showPets.bind(this);
+
+//         const btn = document.querySelector("#animals");
+//         btn.addEventListener("click", this.showPets);
+//     }
+// }
+
+
+// const ob = {
+//     pets : ["kot", "pies", "chomik"],
+// }
+
+// const ob2 = {
+//     pets: ['Elephant', 'Giraffe', 'Tiger']
+// }
+
+
+// function showPets() {
+//     console.log(this.pets);
+// }
+
+// const bindButton = () =>  {
+    
+//     const btn = document.querySelector("#animals");
+//     btn.addEventListener("click", showBigPets);
+// }
+
+// const showSmallPets = showPets.bind(ob)
+// const showBigPets = showPets.bind(ob2)
+// bindButton.call(ob); 
