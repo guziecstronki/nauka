db912f23cc37f8ae8e7330033b7eadab3896ca83
Mateusz Pawlowski <mateuszpawlowski29@gmail.com>
2023-12-29 14:34:49 +0100

    Fundamenty

---
+++ Jakiś darmowy kurs/Odcinek 1/Fundamenty/index.js
@@ -0,0 +1,35 @@
+//Camel case
+//Snake case
+
+//Typy danych
+
+//String
+
+const nameAndSurname = 'Mateusz Pawłowski'
+const name_and_surname = 'Mateusz Pawłowski'
+
+//Number
+
+const number = 10
+
+//Boolean
+
+const trueValue = 1
+const falseValue = 1
+
+//Typowanie statyczne:
+// int number = 10
+
+//Typowanie dynamiczne
+// let number = 10
+
+// Można zmieniać typ danych na jedej zmiennej dzieku dynamicznemu
+
+// Java script jest kompilowany i interpretowany
+// Przez przegladarke interpretowany a na backendzie wyglada to roznie
+
+console.log('Hi!');
+
+console.log(Math.ceil(3.14));
+console.log(Math.ceil('Sialala'));
+// alert('Hello!')
